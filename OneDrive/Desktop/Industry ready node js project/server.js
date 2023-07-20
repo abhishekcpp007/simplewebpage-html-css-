30b4de191c93b52b37c97455c276383762e0209a
Abhishek <akvikramaditya007cppcoder@gmail.com>
2023-07-20 22:13:55 +0545

    Connecting to Mongodb

--- OneDrive/Desktop/Industry ready node js project/server.js
+++ OneDrive/Desktop/Industry ready node js project/server.js
@@ -1,8 +1,23 @@
 const express = require("express")
-const serverConfig= require("./configs/server.config")
+const serverConfig= require('./configs/server.config')
+const mongoose=require('mongoose');
+const dbConfig = require("./configs/db.config");
 
 const app= express();// starting the server
+ 
+mongoose.connect( dbConfig.DB_URL);// FOR this we have mongodb up and running in my local machine
+  const db = mongoose.connection;
+  db.on("error",()=>{
+    console.log("Eroor while connection to DB");
+
+  });
+  db.once("open", ()=>{
+    console.log("Db is connected");
+  })
+
+// logic to connect to MongoDB and create an ADMIN user
+
 
 app.listen(serverConfig.PORT, ()=>{ // application bind to port number 3333  ()- is the call back function
-    console.log(` Server started on the port nomber ${serverConfig.PORT}`);
+    console.log(` Server started on the port number ${serverConfig.PORT}`);
 })
